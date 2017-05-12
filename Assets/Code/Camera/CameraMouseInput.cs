43e65cb5bade9d757204701e0da75949fc246db6
Alexander Verbeek <sanderman@gmail.com>
2017-05-12 10:19:33 +0200

    Create basic scene with a plane, orbital camera, and lighting.

---
+++ Assets/Code/Camera/CameraMouseInput.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Directs mouse input to OrbitCamera
+/// </summary>
+public class CameraMouseInput : MonoBehaviour
+{
+	[SerializeField]
+	private float sensitivity = 1.0f;
+
+	[SerializeField]
+	private OrbitCamera cam;
+
+	private Vector3 prevMousePos;
+
+	void Update()
+	{
+		const int LeftButton = 0;
+		if (Input.GetMouseButton(LeftButton))
+		{
+			// mouse movement in pixels this frame
+			Vector3 mouseDelta = Input.mousePosition - prevMousePos;
+
+			// adjust to screen size
+			Vector3 moveDelta = sensitivity * mouseDelta * (360f / Screen.height);
+
+			cam.Move(moveDelta.x, -moveDelta.y);
+		}
+		prevMousePos = Input.mousePosition;
+	}
+}
---
+++ Assets/Code/Camera/FocusPoint.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Defines angle limits as the maximum deviation away from the rotation of this object.
+/// (in other words: if the yawlimit is 45, then you can only move up to 45 degrees away from this rotation in both directions. 
+/// This means the total angle available would be an angle of 90 degrees)
+/// An angle of 180 allows complete freedom of movement on that axis.
+/// </summary>
+public class FocusPoint : MonoBehaviour
+{
+	[SerializeField]
+	private float yawLimit = 45f;
+
+	[SerializeField]
+	private float pitchLimit = 45;
+
+	public float YawLimit { get { return yawLimit; } }
+	public float PitchLimit { get { return pitchLimit; } }
+}
